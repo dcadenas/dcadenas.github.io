b46f73a30205d953cef1fd1cd6cc2c1313285122
Daniel Cadenas <dcadenas@gmail.com>
2013-11-03 19:31:15 -0200

    Flipping blocks

--- config.rb
+++ config.rb
@@ -9,7 +9,7 @@ activate :blog do |blog|
   # blog.permalink = ":year/:month/:day/:title.html"
   # blog.sources = ":year-:month-:day-:title.html"
   # blog.taglink = "tags/:tag.html"
-  # blog.layout = "layout"
+  blog.layout = "blog_layout"
   # blog.summary_separator = /(READMORE)/
   # blog.summary_length = 250
   # blog.year_link = ":year.html"
@@ -50,7 +50,7 @@ page "/feed.xml", :layout => false
 # page "/path/to/file.html", :layout => false
 # 
 # With alternative layout
-# page "/path/to/file.html", :layout => :otherlayout
+page "/", :layout => :landing_layout
 # 
 # A path which all have the same layout
 # with_layout :admin do
---
+++ source/2013-10-21-blog-redesign.html.markdown
@@ -0,0 +1,6 @@
+---
+title: Blog Redesign
+date: 2013-10-21
+---
+
+I'm currently redesigning my blog. Please check back soon!
